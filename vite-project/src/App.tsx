b4e8dbdd8e6c6476314c4c8f35662db9cf4c366b
jeongho0406 <kjh@skyventures.co.kr>
2025-05-28 11:50:02 +0900

    chore: 파일 정리

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 interface Post {
   id?: number;
@@ -11,7 +8,6 @@ interface Post {
 }
 
 function App() {
-  const [count, setCount] = useState(0);
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
   const [response, setResponse] = useState<Post | null>(null);
@@ -62,26 +58,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <div className="card">
+      <div className="card" style={{ width: "500px" }}>
         <h2>API 테스트 - POST 요청</h2>
         <div style={{ marginBottom: "10px" }}>
           <input
@@ -167,10 +144,6 @@ function App() {
           </div>
         )}
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
